2429ec281db4c0c47a58b2af5800c3b2e461283d
Siddharth Vijayasankar <sidvijay2004@gmail.com>
2019-12-05 19:09:35 -0800

    Added most of login feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import AdminHomePage from './component/AdminHomePage';
+import Login from './component/Login';
+
 
 class App extends Component {
   render () {
     return (
       <div className="container">
-        <AdminHomePage />
+        <Login/>
+
         </div>
     );
   }
