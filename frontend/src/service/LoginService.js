fd75469784c89b67da8473f158b3381b13341532
Siddharth Vijayasankar <sidvijay2004@gmail.com>
2019-12-08 10:36:37 -0800

    Finished Login Feature

---
+++ frontend/src/service/LoginService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+
+const BACKEND_HOST = 'http://localhost:8080'
+
+class LoginService {
+
+
+  isValidLogin(username, password) {
+      return axios.get(`${BACKEND_HOST}/login/`, {
+                  params: {
+                    username: username,
+                    password: password
+                      }
+                    });
+  }
+
+}
+
+
+export default new LoginService()
