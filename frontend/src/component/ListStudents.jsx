c8a66bd765bb351bda68f9c91c8093a8b95aed10
Siddharth Vijayasankar <sidvijay@Siddharths-MacBook-Air.local>
2020-01-07 22:44:36 -0500

    Added checkbox for weekly and monthly

--- frontend/src/component/ListStudents.jsx
+++ frontend/src/component/ListStudents.jsx
@@ -41,7 +41,7 @@ class ListStudents extends Component {
       }
 
       studentReport(studentId) {
-        this.props.history.push(`/StudentReport/${studentId}`)
+        this.props.history.push(`/StudentReport/${studentId}?period=m`)
       }
       deleteStudentClicked(id) {
         this.setState({ message: `Delete of student ${id} starting` })
--- frontend/src/component/StudentReport.jsx
+++ frontend/src/component/StudentReport.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReportService from '../service/ReportService';
+import queryString from 'query-string'
+
 
 
 class StudentReport extends Component {
@@ -7,25 +9,72 @@ class StudentReport extends Component {
   constructor(props) {
 
       super(props)
+
+      const values = queryString.parse(this.props.location.search)
+      console.log(values.period)
       this.state = {
         studentReports: [],
         studentId: this.props.match.params.studentId,
         name: '',
+        period: values.period,
         message: null
       }
-      this.refreshStudents = this.refreshStudents.bind(this)
-
-
+      this.refreshStudentReport = this.refreshStudentReport.bind(this)
+      this.handleCheckWeekly = this.handleCheckWeekly.bind(this)
+      this.handleCheckMonthly = this.handleCheckMonthly.bind(this)
   }
 
   componentDidMount() {
-      this.refreshStudents();
+      this.refreshStudentReport();
   }
 
-  refreshStudents() {
+  handleCheckWeekly() {
+    console.log("Inside handleCheckWeekly");
+
+   //this.props.history.push(`/StudentReport/${this.state.studentId}?period=w`)
+   this.setState({ period: "w"})
+
+
+  ReportService.getStudentWeeklyReport(this.state.studentId)
+      .then(
+          response => {
+              console.log(response);
+              this.setState({ studentReports: response.data})
+
+          }
+      )
+  }
+
+  handleCheckMonthly() {
+    console.log("Inside handleCheckMonthly");
+
+    //this.props.history.push(`/StudentReport/${this.state.studentId}?period=m`)
+    this.setState({ period: "m"})
+
+    ReportService.getStudentMonthlyReport(this.state.studentId)
+        .then(
+            response => {
+                console.log(response);
+                this.setState({ studentReports: response.data})
+
+            }
+        )
+  }
+
+  refreshStudentReport() {
     console.log("Iside refresh stud");
 
+    if(this.state.period == "w"){
+      ReportService.getStudentWeeklyReport(this.state.studentId)
+          .then(
+              response => {
+                  console.log(response);
+                  this.setState({ studentReports: response.data})
 
+              }
+          )
+    }
+    else{
       ReportService.getStudentMonthlyReport(this.state.studentId)
           .then(
               response => {
@@ -34,25 +83,44 @@ class StudentReport extends Component {
 
               }
           )
+      }
   }
 
-  getWeeklyReport(studentId) {
 
-    ReportService.getStudentWeeklyReport(this.state.studentId)
-        .then(
-            response => {
-                console.log(response);
-                this.setState({ studentReports: response.data})
-
-            }
-        )
-      }
 
 render() {
     return (
         <div className="container">
             <h3>Student Report</h3>
             {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
+
+            <div>
+              <li>
+                <label>
+                  <input
+                    type="radio"
+                      value="m"
+                      checked={this.state.period === "m"}
+                      onClick={this.handleCheckMonthly}
+                    />
+                    Monthly
+                  </label>
+              </li>
+
+          <li>
+              <label>
+                <input
+                  type="radio"
+                    value="w"
+                    checked={this.state.period === "w"}
+                    onClick={this.handleCheckWeekly}
+                  />
+                  Weekly
+                  </label>
+            </li>
+
+
+            </div>
 
             <div className="container">
             <table border = "3">
